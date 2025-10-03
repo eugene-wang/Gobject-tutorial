9542c72bb65c34b0bcb2f332bbf4165c1af379cd
Eugene Wang <eugene.wang@apicatech.com>
2025-10-03 15:51:27 +0800

    Initial Commit

---
+++ Gobject-tutorial/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Gobject-tutorial
+//
+//  Created by yeu-jiun wang on 2025/10/2.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
